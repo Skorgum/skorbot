3f0a8f827a9d05d791ddfbdb2677c7750f529a47
Skorgum <skorgum@gmail.com>
2025-11-16 21:29:49 -0500

    Add verbose flag

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
 
@@ -13,16 +14,30 @@ import sys
 def main():
     if len(sys.argv) < 2:
         print("Please enter a prompt")
-        exit(1)
+        sys.exit(1)
+    verbose_flag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
 
     prompt = sys.argv[1]
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if response is None or response.usage_metadata is None:
+        print("response is malformed")
+        return
+    
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
